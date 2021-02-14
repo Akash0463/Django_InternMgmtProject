e9defcbe57d7882ce7a7399772db08499a223e89
Akash0463 <singhal.akash1998@gmail.com>
2021-02-14 19:06:38 +0530

    change

--- InternMgmtApp/views.py
+++ InternMgmtApp/views.py
@@ -8,6 +8,7 @@ from django import forms
 def home(request):
     return render(request,'home.html',{'link':'http://127.0.0.1:8000/register'})
 
+#For Login
 def loginView(request):
     return render(request,'login.html')
 
